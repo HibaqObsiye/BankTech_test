f92b10502fa4fcfddddcd150ccd711bcc0cc9cef
Hibaq Obsiye <hibaqobsiye@Hibaqs-MacBook-Air.local>
2022-11-15 12:55:42 +0000

    account can add transactions and return balance

--- src/account.js
+++ src/account.js
@@ -1,8 +1,19 @@
 class Account{
     constructor() {
         this.balance = 0;
-        
+        this.transactions = []
     }
+
+   sumbitTransaction(date,amount){
+    this.balance += amount;
+    const transaction = {
+      date: date,
+      amount: amount,
+      balance: this.balance
+    }
+    this.transactions.push(transaction);
+  }
+
 
 }
 
